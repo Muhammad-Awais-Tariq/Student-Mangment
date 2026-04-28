57ea23a77736964779e826d0657fd8a587dee7e2
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-04-28 19:35:20 +0500

    added admin page options

--- pages/adminpage.py
+++ pages/adminpage.py
@@ -6,3 +6,9 @@ if "is_admin" not in st.session_state or not st.session_state.is_admin:
 
 st.success(f"you have signed in as {st.session_state.name}")
 st.title("Admin Page")
+
+options = ["Enroll" , "Directory" , "Top Scores","Edit / Drop" , "Analytics "]
+
+option = st.selectbox("Select required functionality", options)
+
+st.write(option)

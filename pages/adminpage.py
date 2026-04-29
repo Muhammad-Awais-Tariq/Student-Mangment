bf2f79a2b23ec8182dd9801f1b2cc961fd1aff9e
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-04-29 16:48:27 +0500

    added student page options

--- pages/adminpage.py
+++ pages/adminpage.py
@@ -10,5 +10,3 @@ st.title("Admin Page")
 options = ["Enroll" , "Directory" , "Top Scores","Edit / Drop" , "Analytics "]
 
 option = st.selectbox("Select required functionality", options)
-
-st.write(option)
--- pages/students.py
+++ pages/students.py
@@ -6,3 +6,7 @@ if "is_student" not in st.session_state or not st.session_state.is_student:
 
 st.success(f"you have signed in as {st.session_state.name}")
 st.title("Student page")
+
+options = ["My Grades" , "My GPA" , "My Stats ","My Rank" , "My Courses"]
+
+option = st.selectbox("Select required functionality", options)

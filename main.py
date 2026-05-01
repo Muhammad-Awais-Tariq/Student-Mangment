114e9111cf09e011116d3e6016f02825d6193dda
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-05-01 12:03:31 +0500

    added student verification via database

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ from pymongo import MongoClient
 
 url = os.getenv('url')
 client = MongoClient(url)
-
 try:
-    database = client.get_database("SMS")
-    students = database.get_collection("Students")
+        database = client.get_database("SMS")
+        students = database.get_collection("Students")
+except Exception as e:
+        raise Exception("Unable to find the document due to the following error: ", e)
+
+def get_students(name , password):
     name_password = students.find({},{"fullName" : 1 , "password" : 1 ,"status" : 1, "_id" : 0} )
     for student in name_password:
-        print(student)
-    client.close()
-except Exception as e:
-    raise Exception("Unable to find the document due to the following error: ", e)
+        if student["status"] == "Active" and name == student["fullName"] and int(password) == student["password"]:
+            return True
+    
+    return False
+
+def main():
+    student = get_students("Hassan Raza" , 141 )
+    print(student)
+if __name__ == "__main__":
+    main()

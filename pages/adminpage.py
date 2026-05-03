c3a4abf4fcd54b79377953f96b9ec02a847357ad
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-05-03 16:02:06 +0500

    added functionality to get marks overview frontend

--- pages/adminpage.py
+++ pages/adminpage.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from main import insert_student , directory
+from main import insert_student , directory , top_scores
 if "is_admin" not in st.session_state or not st.session_state.is_admin:
     st.error("Access denied")
     st.stop()
@@ -7,7 +7,7 @@ if "is_admin" not in st.session_state or not st.session_state.is_admin:
 st.success(f"You have signed in as {st.session_state.name}")
 st.title("Admin Page")
 
-options = ["Enroll" , "Directory" , "Top Scores","Edit / Drop" , "Analytics "]
+options = ["Enroll" , "Directory" , "Scores","Edit / Drop" , "Analytics "]
 
 option = st.selectbox("Select required functionality", options)
 
@@ -127,8 +127,25 @@ if st.session_state.is_selected:
         ]
 
         st.dataframe(course_data)
-    elif option == "Top Scores":
-        st.write("Top Scores")
+    elif option == "Scores":
+        result = top_scores()
+
+        st.markdown("## Scores")
+        st.divider()
+
+        course_data = [
+            {
+                "CourseID": student.get("CourseCode", "N/A"),
+                "MaxStudentName": student.get("MaxStudenName", "N/A"),
+                "CourseName": student.get("CourseName", "N/A"),
+                "MaxObtainedMarks": student.get("MaxObtainedMarks", "N/A"),
+                "MinObtainedMarks": student.get("MinObtainedMarks", "N/A"),
+                "AverageMarks": student.get("AverageMarks", "N/A")
+            }
+            for student in result    
+        ]
+
+        st.dataframe(course_data)
     elif option == "Edit / Drop":
         st.write("Edit / Drop")
     else:
